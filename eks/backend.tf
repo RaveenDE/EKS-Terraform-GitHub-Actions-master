7da06e9adf08712353952410068c8765a2f8b96f
Raveen De Silva <117538343+RaveenDE@users.noreply.github.com>
2024-08-16 13:57:07 +0530

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -16,5 +16,21 @@ terraform {
 }
 
 provider "aws" {
-  region  = var.aws-region
+  region = var.aws_region
+}
+
+# Optional: Create the DynamoDB table for Terraform state locking if it doesn't exist
+resource "aws_dynamodb_table" "terraform_state_lock" {
+  name         = "Lock-Files"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    Name = "Terraform State Lock"
+  }
 }
