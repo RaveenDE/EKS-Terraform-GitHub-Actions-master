baacf84af28fe5c2454e28cc8f725acc23fb174b
Raveen De Silva <117538343+RaveenDE@users.noreply.github.com>
2024-08-16 14:09:01 +0530

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -18,3 +18,18 @@ terraform {
 provider "aws" {
   region  = var.aws-region
 }
+
+resource "aws_dynamodb_table" "terraform_lock" {
+  name           = "Lock-Files"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    Name = "Terraform Lock Table"
+  }
+}
